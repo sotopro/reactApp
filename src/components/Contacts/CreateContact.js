8db62c34b392338f847630f39d2dfe3d5e7cd7ad
Daniel Soto <dasg.1993@gmail.com>
2020-07-05 23:34:30 -0500

    [feature] - create contact form & handle & style components

--- src/components/Contacts/CreateContact.js
+++ src/components/Contacts/CreateContact.js
@@ -1,8 +1,40 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Back from '../../assets/back.svg';
+import './CreateContact.css';
+import ImageInput from '../ImageInput';
+import serializeform from 'form-serialize';
 
 class CreateContact extends Component {
+	handleSubmit = (e) => {
+		const { onCreateContact } = this.props;
+		e.preventDefault();
+		const values = serializeform(e.target, { hash: true });
+		onCreateContact && onCreateContact(values);
+	};
 	render() {
-		return <p>Create Contact</p>;
+		return (
+			<div>
+				<Link className='close-create-contact' to='/'>
+					<img alt='back' className='back-icon' src={Back} />
+				</Link>
+				<form
+					onSubmit={this.handleSubmit}
+					className='create-contact-form'
+				>
+					<ImageInput
+						className='create-contact-avatar-input'
+						name='avatarURL'
+						maxHeight={64}
+					/>
+					<div className='create-contact-details'>
+						<input type='text' name='name' placeholder='Name' />
+						<input type='text' email='name' placeholder='Email' />
+						<button>Add Contact</button>
+					</div>
+				</form>
+			</div>
+		);
 	}
 }
 
